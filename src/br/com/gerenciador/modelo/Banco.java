52dd309404c59ead95ab9ee0184f79b6b4928ad9
Augusto Fabbri <gufabbri@gmail.com>
2019-09-11 17:01:31 -0300

    Adicionando Login e Senha

--- src/br/com/gerenciador/modelo/Banco.java
+++ src/br/com/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static List<Usuario> listaUsuario = new ArrayList<Usuario>();
 	private static Integer chaveSequencial = 1;
 	
 	static {
@@ -18,6 +19,17 @@ public class Banco {
 		empresa2.setId(chaveSequencial++);
 		lista.add(empresa);
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+		u1.setLogin("Augusto");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();
+		u2.setLogin("dani");
+		u2.setSenha("12345");
+		
+		listaUsuario.add(u1);
+		listaUsuario.add(u2);
+		
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -48,6 +60,15 @@ public class Banco {
 		for (Empresa empresa : lista) {
 			if(empresa.getId() == id) {
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for(Usuario usuario : listaUsuario) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
 			}
 		}
 		return null;
