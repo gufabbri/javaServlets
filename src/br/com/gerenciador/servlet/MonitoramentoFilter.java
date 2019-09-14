c4810915675ec70c5cfbb7248f08c9a9056e7e8d
Augusto Fabbri <gufabbri@gmail.com>
2019-09-14 19:00:15 -0300

    adicionaod json/xml

--- src/br/com/gerenciador/servlet/MonitoramentoFilter.java
+++ src/br/com/gerenciador/servlet/MonitoramentoFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 
 //@WebFilter("/entrada")/*todas as requests vao passar pelo filtro*/
 public class MonitoramentoFilter implements Filter {
